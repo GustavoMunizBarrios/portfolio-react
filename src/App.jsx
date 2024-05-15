6b709805ee75367f5ecec7893bade274e94c3b9b
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-05-14 19:11:49 -0600

    Update personal introduction text with language toggle

--- src/App.jsx
+++ src/App.jsx
@@ -169,15 +169,12 @@ function App() {
               alt="profile of Gustavo"
             />
             <p className="text-gray-700 text-lg mt-5 pl-4">
-              Hi! {"I'm "}<span className="font-bold text-[#ef8354]">Gustavo Muñiz</span> I am
-              a web developer and I have 3 years of experience in engineering project
-              development, {"I've"} had the opportunity to work in different areas and
-              develop skills in project management, programming and agile methodologies.
+              {languageToggle ? "Hi! I'm " : "Hola! Soy "}<span className="font-bold text-[#ef8354]">Gustavo Muñiz </span>
+              {languageToggle ? "I am a web developer and I have 3 years of experience in engineering project development, I've had the opportunity to work in different areas and develop skills in project management, programming and agile methodologies." : ", soy desarrollador web y tengo 3 años de experiencia en desarrollo de proyectos de ingeniería, he tenido la oportunidad de trabajar en diferentes áreas y desarrollar habilidades en gestión de proyectos, programación y metodologías ágiles."}
               <br />
               <br />
-              I am focused on <span className="font-bold text-[#1e40af]">Front-end </span
-              > development. I have experience doing multiple projects and have worked
-              in a real development environment with a team of developers.
+              {languageToggle ? "I am focused on " : "Estoy enfocado en desarrollo "} <span className="font-bold text-[#1e40af]">Front-end </span>
+              {languageToggle ? "development. I have experience doing multiple projects and have worked in a real development environment with a team of developers." : ". He ganado experiencia realizando múltiples proyectos y he trabajado en un entorno de desarrollo real con un equipo de desarrolladores."}
             </p>
           </div>
         </section>
