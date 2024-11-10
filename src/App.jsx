02a802b5d2d9af5615b2dc77eb46df5f4355ed1d
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-11-09 21:24:09 -0600

    Moved Experience Section content from App.jsx to ExperienceSection.jsx and updated imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import "./App.css";
 import Header from "./components/Header";
 import profileCompress from "./Images/profileCompress.png";
 import JobIcon from "./assets/JobIcon";
-import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
 import Presentation from "./components/Presentation";
 import ProjectsSection from "./components/ProjectsSection";
+import ExperienceSection from "./components/ExperienceSection";
 
 function App() {
   const [languageToggle, setLanguageToggle] = useState(true);
@@ -47,22 +47,9 @@ function App() {
         <Presentation languageToggle={languageToggle} />
 
         <ProjectsSection languageToggle={languageToggle} />
-        {/* 
-        <!-- ----------------------------- -->
-        <!-- Experience Section - Timeline  -->
-        <!-- ----------------------------- -->
-        */}
-        <section
-          id="experience"
-          className="w-full lg:w-[740px] mx-auto pt-[8rem]"
-        >
-          <h2 className="text-3xl font-semibold text-[#111827] mb-[3rem] flex gap-x-3 items-center dark:text-veryLightGrayishBlue">
-            <JobIcon className="size-8" />
-            {languageToggle ? "Work Experience" : "Experiencia Laboral"}
-          </h2>
 
-          <Timeline languageToggle={languageToggle} />
-        </section>
+        <ExperienceSection languageToggle={languageToggle} />
+
         {/*     
         <!-- ----------------------------- -->
         <!-- ------ About Me section ----- -->
--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,3 +1,16 @@
-export default function ExperienceSection() {
-  return <div>Contenido</div>;
+/* eslint-disable react/prop-types */
+import JobIcon from "../assets/JobIcon";
+import Timeline from "./Timeline";
+
+export default function ExperienceSection({ languageToggle }) {
+  return (
+    <section id="experience" className="w-full lg:w-[740px] mx-auto pt-[8rem]">
+      <h2 className="text-3xl font-semibold text-[#111827] mb-[3rem] flex gap-x-3 items-center dark:text-veryLightGrayishBlue">
+        <JobIcon className="size-8" />
+        {languageToggle ? "Work Experience" : "Experiencia Laboral"}
+      </h2>
+
+      <Timeline languageToggle={languageToggle} />
+    </section>
+  );
 }
