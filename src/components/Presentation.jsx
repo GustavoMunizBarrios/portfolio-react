32b5826d685f0a8026b00be495e7ff92095f9ef2
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-11-15 08:56:18 -0600

    Updated Presentation component with changes to header text and description

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -9,7 +9,7 @@ export default function Presentation({ languageToggle }) {
   return (
     <div className="xl:w-[1080px]">
       <h1 className="text-3xl md:text-4xl lg:text-5xl  font-bold flex flex-row gap-x-4 pb-6 lg:pb-10">
-        {languageToggle ? "Hey, I'm Gustavo" : "Hola, soy Gustavo"}{" "}
+        {languageToggle ? "Hey, I'm Gustavo" : "Hola, soy Gustavo"}
         <a
           href="https://www.linkedin.com/in/developer-gustavo-mu%C3%B1iz-barrios-86708b121/"
           target="_blank"
@@ -22,13 +22,13 @@ export default function Presentation({ languageToggle }) {
         </a>
       </h1>
       <h2 className="text-xl lg:text-2xl text-wrap max-w-[700px] xl:max-w-[1000px]">
-        {languageToggle ? "Welcome! I'm a" : "¡Bienvenido! Soy un"}{" "}
+        {languageToggle ? "I'm a " : "Soy un "}
         <span className="font-bold text-[#EF8354]">
           {languageToggle ? "Web Developer " : "Desarrollador Web "}
         </span>{" "}
         {languageToggle
-          ? "with three years of experience in engineering project development. Feel free to explore my projects to see my skills as a web developer."
-          : "con tres años de experiencia en desarrollo de proyectos de ingeniería. Puedes explorar mis proyectos para ver mis habilidades como desarrollador web."}
+          ? "with an engineering background and a passion for creating digital solutions. After three years leading projects, I specialized in web development, excelling in React and team-based applications. I aim to craft digital experiences that make a difference."
+          : "con experiencia en ingeniería y pasión por crear soluciones digitales. Tras tres años liderando proyectos, me especialicé en programación web, realizando proyectos en equipo. Busco crear experiencias digitales que marquen la diferencia."}
       </h2>
 
       <nav className="flex gap-4 mt-5 flex-wrap">
